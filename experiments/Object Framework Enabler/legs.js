ff76e2c4439108ce49717a71050927f1759e0b4d
Francois Dumont <francois.dumont1@gmail.com>
2024-12-22 20:28:34 +0100

    Formatting

--- experiments/Object Framework Enabler/legs.js
+++ experiments/Object Framework Enabler/legs.js
@@ -1,33 +1,35 @@
 // Leg Class Definition
 class Leg {
-    constructor(legNumber, from, to, targetAltitude) {
-      this.legNumber = legNumber;
-      this.from = from;
-      this.to = to;
-      this.targetAltitude = targetAltitude;
-    }
-  
-    toString() {
-      return `Leg ${this.legNumber}: ${this.from.name} to ${this.to.name}, Target Alt: ${this.targetAltitude} ft`;
-    }
+  constructor(legNumber, from, to, targetAltitude) {
+    this.legNumber = legNumber;
+    this.from = from;
+    this.to = to;
+    this.targetAltitude = targetAltitude;
+  }
+
+  toString() {
+    return `Leg ${this.legNumber}: ${this.from.name} to ${this.to.name}, Target Alt: ${this.targetAltitude} ft`;
+  }
 }
-  
+
 // Pull Data from Google Sheet and Create Legs
 function getLegsFromSheet(waypoints) {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Legs');
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Legs");
   const data = sheet.getRange(2, 1, sheet.getLastRow() - 1, 4).getValues();
 
   let legs = [];
-  data.forEach(row => {
+  data.forEach((row) => {
     const [legNumber, fromName, toName, targetAltitude] = row;
     const fromWaypoint = waypoints[fromName.trim().toUpperCase()];
     const toWaypoint = waypoints[toName.trim().toUpperCase()];
 
     if (fromWaypoint && toWaypoint) {
       let leg = new Leg(legNumber, fromWaypoint, toWaypoint, targetAltitude);
-      legs[leg.legNumber]=leg;
+      legs[leg.legNumber] = leg;
     } else {
-      Logger.log(`Waypoint not found for Leg ${legNumber}: ${fromName} to ${toName}`);
+      Logger.log(
+        `Waypoint not found for Leg ${legNumber}: ${fromName} to ${toName}`
+      );
     }
   });
 
--- experiments/Object Framework Enabler/test.js
+++ experiments/Object Framework Enabler/test.js
@@ -1,14 +1,13 @@
 // Test Function to Run Script
 function testWaypointCreation() {
-    const waypoints = getWaypointsFromSheet();
-    Logger.log(`Created ${waypoints.length} waypoints.`);
-  }
+  const waypoints = getWaypointsFromSheet();
+  Logger.log(`Created ${waypoints.length} waypoints.`);
+}
 
 // Test Function to Run Script
 function testLegCreation() {
-    const waypoints = getWaypointsFromSheet();
-    Logger.log(`Created ${Object.keys(waypoints).length} waypoints.`);
-    const legs = getLegsFromSheet(waypoints);
-    Logger.log(`Created ${legs.length} legs.`);
-  }
-
+  const waypoints = getWaypointsFromSheet();
+  Logger.log(`Created ${Object.keys(waypoints).length} waypoints.`);
+  const legs = getLegsFromSheet(waypoints);
+  Logger.log(`Created ${legs.length} legs.`);
+}
