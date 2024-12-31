953de0e250af40173f08942c6dee69361380c9fa
Francois Dumont <francois.dumont1@gmail.com>
2025-01-01 00:20:22 +0100

    NSTAR-27 GCR server and client test

--- experiments/open-meteo Enabler/API call test/fetchWeatherData.js
+++ experiments/open-meteo Enabler/API call test/fetchWeatherData.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 function fetchWeatherData(latitude, longitude) {
-  const url = `http://127.0.0.1:5000/data?latitude=${latitude}&longitude=${longitude}&variables=temperature_2m&variables=pressure_msl`;
+  const url = `https://fetch-weather-service-crxvm2tubq-od.a.run.app/data?latitude=${latitude}&longitude=${longitude}&variables=temperature_2m&variables=pressure_msl`;
   return fetch(url)
     .then((response) => response.json())
     .then((data) => data);
