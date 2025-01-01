533f1366e556dee46812ea0631940c0e9df93961
Francois Dumont <francois.dumont1@gmail.com>
2025-01-01 15:01:55 +0100

    NSTAR-16 getWeatherVariable

--- src/040_Services/fetchWeatherData.js
+++ src/040_Services/fetchWeatherData.js
@@ -1,34 +1,31 @@
 const fetch = require("node-fetch");
 
-function fetchWeatherData(latitude, longitude) {
+async function fetchWeatherData(latitude, longitude) {
   const url = `https://fetch-weather-service-crxvm2tubq-od.a.run.app/data?latitude=${latitude}&longitude=${longitude}&variables=temperature_2m&variables=pressure_msl`;
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => data);
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-// Export both functions
-module.exports = {
-  fetchWeatherData,
-  getWeatherForDate,
-};
-
-function getWeatherForDate(data, date) {
+function getWeatherVariable(data, date, variable) {
   const result = data.find((entry) => {
     const entryDate = new Date(entry.date).toISOString().slice(0, 19) + "Z"; // Normalize to remove milliseconds
     return entryDate === date;
   });
 
   if (result) {
-    console.log(`Date: ${result.date}`);
-    console.log(`Temperature: ${result.temperature_2m}`);
-    console.log(`Pressure: ${result.pressure_msl}`);
-    return {
-      temperature: result.temperature_2m,
-      pressure: result.pressure_msl,
-    };
+    if (result.hasOwnProperty(variable)) {
+      return result[variable];
+    } else {
+      throw new Error(`Variable "${variable}" not found in data`);
+    }
   } else {
-    console.log("No data found for the given date.");
-    return null;
+    throw new Error(`No data found for date "${date}"`);
   }
 }
+
+// Export both functions
+module.exports = {
+  fetchWeatherData,
+  getWeatherVariable,
+};
